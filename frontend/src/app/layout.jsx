dbaae6bd941a3dde7b8e58e40b830360a7346757
onyekachii <emegwaonyeka@gmail.com>
2024-01-30 12:23:31 +0100

    fixed errors

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -3,59 +3,7 @@ import { Lato, Poppins, Inter } from "next/font/google";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import ReduxProvider from "../components/reduxProvider";
 import { NavProvider } from "../context/nav_context";
-import { WagmiConfig, createConfig, configureChains, } from 'wagmi'
-import '@rainbow-me/rainbowkit/styles.css';
-import {
-  RainbowKitProvider,
-  lightTheme
-} from '@rainbow-me/rainbowkit';
-import { publicProvider } from 'wagmi/providers/public'
-import { connectorsForWallets } from '@rainbow-me/rainbowkit';
-import {
-  metaMaskWallet,
-  trustWallet,
-  injectedWallet,
-  rainbowWallet,
-  walletConnectWallet,
-  coinbaseWallet,
-} from '@rainbow-me/rainbowkit/wallets';
-import { polygonZkEvm, polygonZkEvmTestnet } from 'wagmi/chains'
-import { XRPLDevnet } from './Chain.tsx'
-
-const projectId = "274de4271228fdd69013c56274f0e688";
-const { chains, publicClient } = configureChains(
-  [ polygonZkEvm, polygonZkEvmTestnet, XRPLDevnet],
-  [
-    publicProvider()
-  ]
-);
-
-const connectors = connectorsForWallets([
-  {
-    groupName: 'Recommended',
-    wallets: [
-      metaMaskWallet({ projectId, chains }),
-      trustWallet({ projectId, chains }),
-    ]
-  },
-  {
-    groupName: 'Others',
-    wallets: [
-      coinbaseWallet({ chains, 
-        appName: 'Verxio | Decentralizing the future of work!' }),
-      injectedWallet({ chains }),
-      rainbowWallet({ projectId, chains }),
-      walletConnectWallet({ projectId, chains }),
-    ],
-  },
-]);
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient
-})
-
+import WagmiProviders from "../components/wagmiProviders";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -75,23 +23,11 @@ export default async function RootLayout({ children }) {
       <body className={`${inter.variable} font-inter`}>
         <NavProvider>
           <ReduxProvider>
-          <WagmiConfig config={wagmiConfig}>
-              <RainbowKitProvider 
-                theme={lightTheme({
-                  accentColor: '#1570ef',
-                  accentColorForeground: 'white',
-                  borderRadius: 'small',
-                  fontStack: 'system',
-                  overlayBlur: 'small'
-                },
-                )} chains={chains}>
-            <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
-            </RainbowKitProvider>
-        </WagmiConfig>
-
-      </ReduxProvider>
-  </NavProvider>
-
+            <WagmiProviders>
+              <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+            </WagmiProviders>
+          </ReduxProvider>
+        </NavProvider>
       </body>
     </html>
   );
