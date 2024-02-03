58eb6d9be90acd3d28d7474b98f5cf2f8f3ed295
onyekachii <emegwaonyeka@gmail.com>
2024-02-03 14:55:30 +0100

    fixing error 2

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
-import { Logo, Profile, Search } from "./atoms";
+import { Logo, Profile } from "./atoms";
 
-const Header = async () => {
+const Header = () => {
   return (
     <section className="px-[4%] w-full font-body shadow-dashboard-header h-fit sticky top-0 left-0  py-[19px] gap-4 flex items-center justify-between shadow-header bg-primary_white border-[#D2D2D2] bg-[#FFFFFF] z-50">
       <Logo className="lg:hidden" />
